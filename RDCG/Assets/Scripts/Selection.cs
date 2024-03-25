43951e78ac55d69f4cff26343fba17c42b9b0d3a
leejisu0909 <leejisu0909@naver.com>
2024-03-25 16:52:37 +0900

    MainTitle,CharacterSelection1,2,Ending 스크립트 Selection으로 통합

---
+++ RDCG/Assets/Scripts/Selection.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Selection : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //MainTitle에서 게임시작 버튼 클릭 시 CharacterSelection1으로 이동
+    public void MainTitleClickStartBtn(){
+        SceneManager.LoadScene("CharacterSelection1");
+    }
+
+    // CharacterSelection1화면에서 캐릭터 버튼 클릭시 CharacterSelection2로 이동
+    public void CharacterSelection1ClickChar1Btn(){
+        SceneManager.LoadScene("CharacterSelection2");
+    }
+
+    // CharacterSelection1화면에서 뒤로가기 버튼 클릭시 MainTitle로 이동
+     public void CharacterSelection1ClickBackBtn(){
+        SceneManager.LoadScene("MainTitle");
+    }
+
+    //CharacterSelection2에서 뒤로가기버튼 클릭 시 CharacterSelection1으로 이동
+    public void CharacterSelection2ClickBackBtn(){
+        SceneManager.LoadScene("CharacterSelection1");
+    }
+
+    //게임 클리어 or 게임 오버 화면에서 메인화면 버튼 클릭시 MainTitle로 이동
+    public void Ending(){
+        SceneManager.LoadScene("MainTitle");
+    }
+}
