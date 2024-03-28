298579dcff67565410976814c0cdde9339c74231
Yu <sunny7360@naver.com>
2024-03-28 20:34:18 +0900

    Card, Deck 스크립트 작성 및 CSV 파싱 작업
    Card로 생성자 Deck에 카드 리스트 공격카드 10장이 있고 나중 CSV 파일로 수정 할 예정

---
+++ RDCG/Assets/Scripts/Card.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using UnityEngine;
+
+public class Card
+{
+    //Get Set 프로퍼티 (개방폐쇄형)
+    public string cardName { get; private set; } //카드의 이름을 저장하는 변수
+    public int cardCost { get; private set; } //카드의 코스트를 저장하는 변수
+    public int cardValue { get; private set; } //카드의 데미지나 방어를 저장하는 능력치 변수
+    public string cardContent { get; private set; } //카드의 정보를 설명하는 변수
+
+    //생성자
+    public Card(string name, int cost, int value, string content)
+    {
+        this.cardName = name; // 카드 이름 초기화
+        this.cardCost = cost; // 카드 코스트 초기화
+        this.cardValue = value; // 카드 벨류 초기화
+        this.cardContent = content; // 카드 내용 초기화
+    }
+}
---
+++ RDCG/Assets/Scripts/Deck.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deck : MonoBehaviour
+{
+    // 카드를 담는 리스트를 선언
+    public List<Card> cardDeck = new List<Card>();
+
+    /// <summary>
+    /// 카드를 추가하는 함수 나중에 CSV로 데이터를 불러 받아서 기능 구현이 되면 수정 할 예정
+    /// 총 덱은 10장이 있는데 5장은 1데미지 1코스트, 3장은 2데미지 2코스트, 2장은 3데미지 3코스트
+    /// 나중에 new Card 할 때 Attack 인지 Defense 일지 CardType를 정해주는 것을 Enum 클래스로 타입을 정함
+    /// </summary>
+    public void CardAdd()
+    {
+        cardDeck.Add(new Card("공격카드 1", 1, 1, "적에게 데미지 1를 줍니다."));
+        cardDeck.Add(new Card("공격카드 1", 1, 1, "적에게 데미지 1를 줍니다."));
+        cardDeck.Add(new Card("공격카드 1", 1, 1, "적에게 데미지 1를 줍니다."));
+        cardDeck.Add(new Card("공격카드 1", 1, 1, "적에게 데미지 1를 줍니다."));
+        cardDeck.Add(new Card("공격카드 1", 1, 1, "적에게 데미지 1를 줍니다."));
+        cardDeck.Add(new Card("공격카드 2", 2, 2, "적에게 데미지 2를 줍니다."));
+        cardDeck.Add(new Card("공격카드 2", 2, 2, "적에게 데미지 2를 줍니다."));
+        cardDeck.Add(new Card("공격카드 2", 2, 2, "적에게 데미지 2를 줍니다."));
+        cardDeck.Add(new Card("공격카드 3", 3, 3, "적에게 데미지 3를 줍니다."));
+        cardDeck.Add(new Card("공격카드 3", 3, 3, "적에게 데미지 3를 줍니다."));
+    }
+}
