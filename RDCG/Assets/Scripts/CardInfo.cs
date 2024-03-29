18ea32763e65f2d42ea79acde8506141fb102eda
dongkeunkeun <162666242+dongkeunkeun@users.noreply.github.com>
2024-03-29 22:58:56 +0900

    카드의 코스트 데미지등 정보 받아와 사용시 적용되도록 수정
    CardInfo스크립트 만들어서 카드의 정보를 카드의 프리팹마다 넣을수있게 만들고 cardPosition스크립트 수정하여 카드 사용시 코스트와 데미지 정보 받아와 사용하게 만듭니다.

---
+++ RDCG/Assets/Scripts/CardInfo.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardInfo : MonoBehaviour
+{
+    // 카드의 속성을 설정할 Card 컴포넌트
+    private Card cardComponent;
+    // 카드의 이름
+    public string cardName;
+    // 카드의 코스트
+    public int cardCost;
+    // 카드의 값
+    public int cardValue;
+    // 카드의 내용
+    public string cardContent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Card 클래스의 생성자를 사용하여 카드 생성 및 속성 설정
+        Card cardComponent = new Card(cardName, cardCost, cardValue, cardContent);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
