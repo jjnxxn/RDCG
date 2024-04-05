4f82cc18ed63681ff25a871441e57784154ce979
dongkeunkeun <162666242+dongkeunkeun@users.noreply.github.com>
2024-04-05 22:11:25 +0900

    카드생성시 덱에 카드 추가 및 리스트에 남은 카드 없을시 기본카드 생성
    모든카드를 다 써서 리스트에 카드가없을시 기본카드가 무한으로 생성되게 만들었고 만약 새로운 카드를 보상으로 얻게될시 스테이지 시작시 새로운 카드를 리스트에 추가하도록 수정하였습니다

--- RDCG/Assets/Scripts/cardPosition.cs
+++ RDCG/Assets/Scripts/cardPosition.cs
@@ -4,9 +4,6 @@ using UnityEngine;
 
 public class cardPosition : MonoBehaviour
 {
-    // Deck 클래스의 인스턴스 생성
-    public Deck deck;
-
     // 카드의 고정 위치를 받아오기 위한 변수 지정
     public GameObject cardPosition1;
     public GameObject cardPosition2;
@@ -14,29 +11,47 @@ public class cardPosition : MonoBehaviour
     public GameObject cardPosition4;
     public GameObject cardPosition5;
 
-    // 나중에 삭제할 코드 하지만 지금은 dex클래스에서 카드생성기능 없어서 임시로 사용
-    public List<GameObject> cards;
+    // 현재스테이지에서 사용할 카드을 담은 리스트 
+    public List<GameObject> cards; 
     // 카드 복사본 배열
     private GameObject[] cardCopies;
+
+    // 남은 카드가 없을때 사용할 기본카드
+    public GameObject normalCard;
 
     // 코루틴위해 사용할 인덱스번호
     private int index = 0;
 
     //코스트 부족 테스트를 위한 임시 변수
-    private int testCost = 2;
+    private int testCost = 1000;
 
     void Start()
     {
-        // Deck 클래스의 인스턴스 생성
-        deck = new Deck();
-        // Deck 클래스의 CardAdd 함수 호출하여 카드덱 생성
-        deck.CardAdd();
-
         // 카드 위치를 배열로 선언
         GameObject[] cardPositions = new GameObject[] { cardPosition1, cardPosition2, cardPosition3, cardPosition4, cardPosition5 };
 
         // 카드 복사본 배열 초기화
         cardCopies = new GameObject[cardPositions.Length];
+
+        // CurrentDeck스크립트 불러옴
+        CurrentDeck currentDeck = FindObjectOfType<CurrentDeck>();
+
+        // null 체크를 수행하여 currentDeck가 null이 아닌지 확인합니다.
+        if (currentDeck != null && currentDeck.cardDeck != null)
+        {
+            // foreach문 사용하여 카드덱에 있는 카드들 cards리스트에 추가
+            foreach (GameObject card in currentDeck.cardDeck)
+            {
+                // 잘 추가되는지 확인하기위한 로그
+                Debug.Log("카드가 추가되었습니다");
+                cards.Add(card);
+            }
+        }
+        else
+        {
+            Debug.Log("추가할 카드가 없습니다");
+        }
+
 
         for (int i = 0; i < cardPositions.Length; i++)
         {
@@ -48,13 +63,8 @@ public class cardPosition : MonoBehaviour
             cards.RemoveAt(randomIndex);
             // 카드 복사본 배열에 추가
             cardCopies[i] = cardCopy;
-
-            /* deck클래스에서 리스트에 카드 추가하는함수 만들시 이걸로 변경
-            int randomIndex = Random.Range(0, deck.cardDeck.Count);
-            GameObject cardCopy = Instantiate(deck.cardDeck[randomIndex].gameObject, cardPositions[i].transform.position, Quaternion.identity);
-            deck.cardDeck.RemoveAt(randomIndex);
-            cardCopies[i] = cardCopy; */
         }
+
     }
 
     void Update()
@@ -63,22 +73,27 @@ public class cardPosition : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Alpha1)) //키보드숫자1번 누를시
         {
             UseCardAtPosition(cardPosition1.transform.position); //1번카드포지션위치를 매개변수로 넣어 함수실행
+            Debug.Log("1");
         }
         else if (Input.GetKeyDown(KeyCode.Alpha2)) //키보드2번 누를시
         {
             UseCardAtPosition(cardPosition2.transform.position); //2번카드포지션위치를 매개변수로 넣어 함수실행
+            Debug.Log("2");
         }
         else if (Input.GetKeyDown(KeyCode.Alpha3)) //키보드3번 누를시
         {
             UseCardAtPosition(cardPosition3.transform.position); //3번카드포지션위치를 매개변수로 넣어 함수실행
+            Debug.Log("4");
         }
         else if (Input.GetKeyDown(KeyCode.Alpha4)) //키보드4번 누를시
-        {
+        {   
             UseCardAtPosition(cardPosition4.transform.position); //4번카드포지션위치를 매개변수로 넣어 함수실행
+            Debug.Log("3");
         }
         else if (Input.GetKeyDown(KeyCode.Alpha5)) //키보드5번 누를시
         {
             UseCardAtPosition(cardPosition5.transform.position); //5번카드포지션위치를 매개변수로 넣어 함수실행
+            Debug.Log("5");
         }
     }
 
@@ -127,20 +142,25 @@ public class cardPosition : MonoBehaviour
         // 일정 시간 동안 대기 시간바꾸어도 상관없음
         yield return new WaitForSeconds(2.0f);
 
-        // 사용할 카드의 랜덤선택
-        int randomIndex = Random.Range(0, cards.Count);
-        //  마찬가지로 나중에 바꾸어야할 부분
-        //  int randomIndex = Random.Range(0, deck.cardDeck.Count);
+        //새롭개 생성할 카드를 받아올 변수
+        GameObject newCardCopy;
 
-        // 리스트에서 랜덤 카드를 매개변수 위치에 복사 
-        GameObject newCardCopy = Instantiate(cards[randomIndex], position, Quaternion.identity);
-        // 마찬가지로 나중에 바꾸어야할 부분
-        //GameObject newCardCopy = Instantiate(deck.cardDeck[randomIndex].gameObject, position, Quaternion.identity);
+        if (cards.Count > 0)
+        {
+            // 사용할 카드의 랜덤선택
+            int randomIndex = Random.Range(0, cards.Count);
+  
+            // 리스트에서 랜덤 카드를 매개변수 위치에 복사 
+            newCardCopy = Instantiate(cards[randomIndex], position, Quaternion.identity);
 
-        // 생성된 카드는 리스트에서 제거
-        cards.RemoveAt(randomIndex);
-        // 마찬가지로 나중에 바꾸어야할 부분
-        //deck.cardDeck.RemoveAt(randomIndex);
+            // 생성된 카드는 리스트에서 제거
+            cards.RemoveAt(randomIndex);   
+        }
+        else
+        {
+            // 카드 리스트에 더 이상 카드가 없을 경우 기본 카드 생성
+            newCardCopy = Instantiate(normalCard, position, Quaternion.identity);
+        }
 
         // 카드 복사본 배열에 생성된 카드로 변경
         cardCopies[index] = newCardCopy;
