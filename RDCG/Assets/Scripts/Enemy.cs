597f1f291c44dcc9866e29d7c8a59542ef227c67
Yu <sunny7360@naver.com>
2024-04-13 03:00:05 +0900

    적 AI 행동 패턴 추가 및 애니메이션 추가
    5턴 째 적의 체력이 최대 체력의 절반만큼 차게 함 / 9턴에 경고 메세지와 함께 강력한 공격 애니메이션 구현 / 10턴에 강력한 공격 구현

--- RDCG/Assets/Scripts/Enemy.cs
+++ RDCG/Assets/Scripts/Enemy.cs
@@ -1,14 +1,21 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Enemy : MonoBehaviour
 {
-    public float enemyHp = 30; // 적의 현재 체력
-    public float enemyMaxHp = 30; // 적의 최대 체력
+    public float enemyHp; // 적의 현재 체력
+    public float enemyMaxHp; // 적의 최대 체력
+
     public Text enemyHpText; // 적의 체력을 알 수 있는 체력 텍스트
     public Slider enemyHpBar; // 현재 적의 체력 바 UI 
+    public Animation enemyWarning; //적이 강력한 공격을 하는 애니메이션
+    public Text enemyWarningText; // 적이 강력한 공격을 할 때 나오는 텍스트
+
+    public int enemyAttackDamage; // 적이 플레이어에게 줄 수 있는 데미지
     public int enemyRandomDamage; // 적이 플레이어에게 줄 수 있는 랜덤 데미지 (적의 행동 패턴을 다양하게)
 
     public Player player; // 플레이어 스크립트
@@ -18,7 +25,8 @@ public class Enemy : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        enemyMaxHp = 30;
+        enemyWarningText.enabled = false;
     }
 
     // Update is called once per frame
@@ -28,10 +36,51 @@ public class Enemy : MonoBehaviour
         enemyHpBar.value = enemyHp / enemyMaxHp; // HP 슬라이더 바 현재 체력 비례하여 최대 체력으로 나눠 업데이트
     }
 
+    /// <summary>
+    /// 적이 플레이어의 체력을 1부터 10으로 랜덤하게 데미지를 주는 함수
+    /// </summary>
     public void EnemyAttack()
     {
-        enemyRandomDamage = Random.Range(1, 11); // 에너미는 플레이어의 체력을 랜덤적으로 1~10까지 까이게 함
-        player.playerHp -= enemyRandomDamage;
+        enemyRandomDamage = Random.Range(1, 11); // 적은 플레이어의 체력을 랜덤적으로 1~10까지 까이게 함
+        player.playerHp -= enemyRandomDamage; // 플레이어의 체력을 적의 랜덤 데미지만큼 까이게 함
         Debug.Log("플레이어의 현재 체력은 " + player.playerHp);
+    }
+
+    /// <summary>
+    /// 적이 5턴일 경우 현재 체력에서 최대 체력 절반만큼 차게 하는 함수
+    /// </summary>
+    public void EnemyHeal()
+    {
+        Debug.Log("적의 체력이 회복되었습니다.");
+        enemyHp += enemyMaxHp;
+
+        if (enemyHp >= enemyMaxHp)
+        {
+            enemyHp = enemyMaxHp;
+        }
+    }
+
+    /// <summary>
+    /// 적의 턴이 10턴이 되었을 경우 플레이어에게 강력한 공격을 한번 주는 함수
+    /// </summary>
+    public void EnemyLastAttack()
+    {
+        enemyAttackDamage = 30; // 강력한 공격은 30 데미지
+        player.playerHp -= enemyAttackDamage; // 플레이어 체력을 30만큼 까이게 함
+        Debug.Log("플레이어의 현재 체력은 " + player.playerHp);
+    }
+
+    /// <summary>
+    /// 적이 강력한 공격을 할 때 나오는 경고창 UI 패널로 나옴
+    /// </summary>
+    public IEnumerator EnemyWarning()
+    {
+        enemyWarningText.enabled = true; // 경고 메세지 보이게 함
+        enemyWarningText.text = "적이 강력한 공격을 \n준비중입니다...";
+        enemyWarning.Play(); // 적 경고 애니메이션 실행
+
+        yield return new WaitForSeconds(1.0f); // 1초 대기 후
+
+        enemyWarningText.enabled = false; // 경고 메세지 사라지게 실행
     }
 }
--- RDCG/Assets/Scripts/TurnManager.cs
+++ RDCG/Assets/Scripts/TurnManager.cs
@@ -6,6 +6,8 @@ using UnityEngine.UI;
 public class TurnManager : MonoBehaviour
 {
     public Button turnBtn; // 턴종료 버튼 누르면 적의 턴으로 넘어가는 UI
+    public int enemyTurn; // 적의 턴이 몇번 진행되었는지 알 수 있는 변수
+    
     public bool isPlayerTurn; // 현재 플레이어의 턴상태 확인
     public bool isEnemyTurn; // 현재 적의 턴상태 확인
 
@@ -16,6 +18,7 @@ public class TurnManager : MonoBehaviour
     void Start()
     {
         turnBtn.GetComponent<Button>();
+        enemyTurn = 1;
     }
 
     // Update is called once per frame
@@ -35,7 +38,29 @@ public class TurnManager : MonoBehaviour
         isEnemyTurn = true; // 적 턴이 실행
 
         turnBtn.interactable = false; // 턴 종료 버튼을 비활성화
-        enemy.EnemyAttack(); // 적 스크립트에 있는 공격 함수 실행
+        enemyTurn++; // 적의 턴 증가 (적의 체력 회복이나 강력한 공격 준비)
+        Debug.Log(enemyTurn);
+        
+        // 적의 턴이 5턴이 진행되었을 경우
+        if (enemyTurn == 5)
+        {
+            enemy.EnemyHeal(); // 적의 체력을 회복하는 함수 실행
+        }
+        // 적의 턴이 9턴이 진행 되었을 경우
+        else if (enemyTurn == 9)
+        {
+            StartCoroutine(enemy.EnemyWarning()); // 적이 강력한 공격을 할 것이라는 경고 애니메이션 (추후 UI 할 때 수정)
+        }
+        // 적의 턴이 10턴이 진행 되었을 경우
+        else if (enemyTurn == 10)
+        {
+            enemy.EnemyLastAttack(); // 적이 강력한 공격인 플레이어 체력 30을 깎는 함수 실행
+        }
+
+        else
+        {
+            enemy.EnemyAttack(); // 적 스크립트에 있는 공격 함수 실행
+        }
 
         isPlayerTurn = true; // 플레이어 턴 시작
         isEnemyTurn = false; // 적 턴이 종료
